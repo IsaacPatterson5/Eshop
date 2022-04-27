5eaf3061e141602fe2d3d83ffc56bfa3da607844
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-04-26 17:16:44 -0700

    Setback to Video 25

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
   const [products, setProducts] = useState([
@@ -6,15 +6,8 @@ function App() {
     {name: 'product2', price: 200.00},
   ]);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
   function addProduct() {
-    setProducts(prevState => [...prevState, 
-      {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+    setProducts([...products, {name: 'product3', price: 300.00}])
   }
 
   return (
