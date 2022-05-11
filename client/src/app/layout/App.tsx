0cba12d7e6e8c810635f993676160f474d3886d3
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-05-11 16:26:54 -0700

    Finished Video 31

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import { Product } from "../models/product";
@@ -25,7 +26,7 @@ function App() {
 
   return (
     <div>
-      <h1>Eshop</h1>
+      <Typography variant='h1'>Eshop</Typography>
       <Catalog products={products} addProduct={addProduct}/>
     </div>
   );
