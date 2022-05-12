ec94cdcb4f0ccda80bff6f926479ad1456697ee4
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-05-11 20:27:42 -0700

    Finished Video 37

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,38 +1,27 @@
-import { Container, CssBaseline } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Container, createTheme, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
 import Header from "./header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-  function addProduct() {
-    setProducts(prevState => [...prevState, 
-      {
-        id: prevState.length + 101,
-        name: 'product' + (prevState.length + 1), 
-        price: (prevState.length * 100) + 100,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: 'http://picsum.photos/200'
-      }])
-  }
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light'
+  const theme = createTheme({
+    palette: {
+      mode: paletteType
+    }
+  })
+  
 
   return (
-    <>
-      <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
       <Header/>
       <Container>
-      <Catalog products={products} addProduct={addProduct}/>
+      <Catalog/>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
