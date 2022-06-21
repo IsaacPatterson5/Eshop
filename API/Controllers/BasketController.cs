8b14dd40f7c71bd2e21dab45c02f96a96112adde
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-06-21 09:53:34 -0700

    Finished Video 64

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        public StoreContext Context { get; }
+        public BasketController(StoreContext context)
+        {
+            Context = context;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await Context.Baskets
+                .Include(i => i.Items)
+                .ThenInclude(p => p.Product)
+                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+
+                if (basket == null) return NotFound();
+
+                return basket;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        {
+            // get basket
+            // create basket
+            // get prodyct
+            // add item
+            // save changes
+            return StatusCode(201);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> RemoveBasketItem (int productId, int quantity)
+        {
+            // get basket
+            // remove item or reduce quantity
+            // save changes
+            return Ok();        
+        }
+
+    }
+}
