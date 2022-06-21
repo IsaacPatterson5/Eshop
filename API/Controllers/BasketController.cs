156af86c6e1232ec9ec145d5f311a63e747d7dad
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-06-21 11:33:23 -0700

    Finished Video 66

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -22,25 +22,30 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<Basket>> GetBasket()
         {
-            var basket = await Context.Baskets
-                .Include(i => i.Items)
-                .ThenInclude(p => p.Product)
-                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+            var basket = await RetrieveBasket();
 
-                if (basket == null) return NotFound();
+            if (basket == null) return NotFound();
 
-                return basket;
+            return basket;
         }
 
-        [HttpPost]
+        [HttpPost] // api/basket?productId=3&quantity=2
         public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
         {
-            // get basket
-            // create basket
-            // get prodyct
-            // add item
-            // save changes
-            return StatusCode(201);
+            var basket = await RetrieveBasket();
+
+            if (basket == null) basket = CreateBasket();
+
+            var product = await Context.Products.FindAsync(productId);
+            if (product == null) return NotFound();
+
+            basket.AddItem(product, quantity);
+
+            var result = await Context.SaveChangesAsync() > 0;
+
+            if (result) return StatusCode(201);
+            
+            return BadRequest(new ProblemDetails{Title = "Problem saving item to basket"});
         }
 
         [HttpDelete]
@@ -50,6 +55,27 @@ namespace API.Controllers
             // remove item or reduce quantity
             // save changes
             return Ok();        
+        }
+
+
+        private async Task<Basket> RetrieveBasket()
+        {
+            return await Context.Baskets
+                .Include(i => i.Items)
+                .ThenInclude(p => p.Product)
+                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+        }
+
+        private Basket CreateBasket()
+        {
+            var buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions{IsEssential = true, Expires = DateTime.Now.AddDays(30)};
+            Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+            var basket = new Basket{BuyerId = buyerId};
+            Context.Baskets.Add(basket);
+            return basket;
+
+
         }
 
     }
