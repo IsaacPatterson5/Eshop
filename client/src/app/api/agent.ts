6026666e7d1fed6fceb336d733074d91a656c3e7
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-06-22 09:06:25 -0700

    Finished Video 70

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -5,6 +5,8 @@ import { history } from "../..";
 const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.withCredentials = true;
+
 
 interface ResponseData {
     data: {
@@ -71,9 +73,16 @@ const Testerrors = {
     getValidationError: () => requests.get('buggy/validation-error'),
 }
 
+const Basket = {
+  get: () => requests.get('basket'),
+  addItem: (productId: number, quantity = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+  removeItem: (productId: number, quantity = 1) => requests.delete(`basket?productId=${productId}&quantity=${quantity}`,),
+}
+
 const agent = {
     Catalog,
-    Testerrors
+    Testerrors,
+    Basket
 }
 
 export default agent;
