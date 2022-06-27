7b702380d2cb7e683f735374ac8951e4d81f61a0
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-06-27 15:36:02 -0700

    Finished Video 106

---
+++ API/RequestHelpers/ProductParams.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.RequestHelpers
+{
+    public class ProductParams : PaginationParams
+    {
+        public int OrderBy { get; set; }
+        public int searchTerm { get; set; }
+        public int Types { get; set; }
+        public int Brands { get; set; }
+        
+    }
+}
