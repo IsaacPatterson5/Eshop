868b656aeb80f00bf262b6248965a894d65c9e95
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-06-28 10:21:43 -0700

    Finished Video 110

--- client/src/features/catalog/CatalogSlice.ts
+++ client/src/features/catalog/CatalogSlice.ts
@@ -26,12 +26,26 @@ export const fetchProductAsync = createAsyncThunk<Product, number>(
         }
     }
 )
+
+export const fetchFilters = createAsyncThunk(
+    'catalog/fetchFilters',
+    async (_, thunkAPI) => {
+        try {
+            return agent.Catalog.fetchFilters();   
+        } catch (error) {
+            return thunkAPI.rejectWithValue({error: error.data});
+        }
+    }
+)
  
 export const CatalogSlice = createSlice({
     name: 'catalog',
     initialState: productsAdapter.getInitialState({
         productsLoaded: false,
-        status: 'idle'
+        filtersLoaded: false,
+        status: 'idle',
+        brands: [],
+        types: []
     }),
     reducers: {},
     extraReducers: (builder => {
@@ -57,7 +71,20 @@ export const CatalogSlice = createSlice({
         builder.addCase(fetchProductAsync.rejected, (state, action) => {
             console.log(action);
             state.status = 'idle';
-        })
+        });
+        builder.addCase(fetchFilters.pending, (state) => {
+            state.status = 'pendingFetchFilters';
+        });
+        builder.addCase(fetchFilters.fulfilled, (state, action) => {
+            state.status = 'fufilled';
+            state.brands = action.payload.brands;
+            state.types = action.payload.types;
+            state.filtersLoaded = true;
+        });
+        builder.addCase(fetchFilters.rejected, (state, action) => {
+            state.status = 'idle';
+            console.log(action.payload);
+        })    
     })
 })
 
