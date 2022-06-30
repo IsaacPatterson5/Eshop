1f247d26009ffcfc4ac9f7ea422c253fb079e146
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-06-30 10:18:43 -0700

    Finished Video 135

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,39 +1,26 @@
-import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import agent from '../../app/api/agent';
-
-const theme = createTheme();
+import { FieldValues, useForm } from 'react-hook-form';
+import { LoadingButton } from '@material-ui/lab';
 
 export default function Login() {
-  const [values, setValues] = useState({
-    username: '',
-    password: ''
-  })
+  const {register, handleSubmit, formState: {isSubmitting}} = useForm()
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-   agent.Account.login(values);
-  };
-
-  function handleInputChange(event: any) {
-    const {name, value} = event.target;
-    setValues({...values, [name]: value});
+  async function SubmitForm(data: FieldValues) {
+    await agent.Account.login(data);
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component={Paper} maxWidth="sm" 
+      <Container
+          component={Paper} maxWidth="sm" 
           sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', p: 4}}>
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
@@ -41,33 +28,29 @@ export default function Login() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box component="form" onSubmit={handleSubmit(SubmitForm)} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               fullWidth
               label="Username"
-              name="username"
               autoFocus
-              onChange={handleInputChange}
-              value={values.username}
+              {...register('username')}
             />
             <TextField
               margin="normal"
               fullWidth
-              name="password"
               label="Password"
               type="password"
-              onChange={handleInputChange}
-              value={values.password}
+              {...register('password')}
             />
-            <Button
+            <LoadingButton
+              loading={isSubmitting}
               type="submit"
               fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
+              variant="contained" sx={{ mt: 3, mb: 2 }}
+              >
               Sign In
-            </Button>
+            </LoadingButton>
             <Grid container>
               <Grid item>
                 <Link to='/register'>
@@ -77,6 +60,5 @@ export default function Login() {
             </Grid>
           </Box>
       </Container>
-    </ThemeProvider>
   );
 }
