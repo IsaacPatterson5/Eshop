b3af42ce9d8f59132ad74e0126c076a8f565fde6
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-06-30 08:57:35 -0700

    Finished Video 132

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -86,10 +86,17 @@ const Basket = {
   removeItem: (productId: number, quantity = 1) => requests.delete(`basket?productId=${productId}&quantity=${quantity}`,),
 }
 
+const Account = {
+  login: (values: any) => requests.post('account/login', values),
+  register: (values: any) => requests.post('account/register', values),
+  currentUser: (values: any) => requests.post('account/login', values),
+}
+
 const agent = {
     Catalog,
     Testerrors,
-    Basket
+    Basket,
+    Account
 }
 
 export default agent;
