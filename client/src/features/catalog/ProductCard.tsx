d76aef357a13a5d41a2974f91859409042c897da
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-07-01 16:15:24 -0700

    Finished Videos 156/157

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,39 +1,52 @@
 import { LoadingButton } from "@material-ui/lab";
-import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import { Product } from "../../app/models/product";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { currencyFormat } from "../../app/util/util";
 import { addBasketItemAsync } from "../basket/basketSlice";
 
 interface Props {
-  product: Product
+  product: Product;
 }
-export default function ProductCard({ product }: Props) {
-  const { status } = useAppSelector(state => state.basket);
+
+const ProductCard = ({ product }: Props) => {
+  const { status } = useAppSelector((state) => state.basket);
   const dispatch = useAppDispatch();
 
   return (
     <Card>
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: 'secondary.main' }}>
-            {product.name.charAt(0).toUpperCase()}
+          <Avatar sx={{ bgcolor: "secondary.main" }}>
+            {product.name.charAt(0)}
           </Avatar>
         }
         title={product.name}
         titleTypographyProps={{
-          sx: { fontWeight: 'bold', color: 'primary.main' }
+          sx: { fontWeight: "bold", color: "primary.main" },
         }}
       />
       <CardMedia
-        sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.dark' }}
         image={product.pictureUrl}
         title={product.name}
+        sx={{
+          height: 140,
+          backgroundSize: "contain",
+          bgcolor: "primary.dark",
+        }}
       />
       <CardContent>
-        <Typography gutterBottom color='secondary' variant="h5">
-          {currencyFormat(product.price)}
+        <Typography gutterBottom variant="h5" color="secondary">
+          ${(product.price / 100).toFixed(2)}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {product.brand} / {product.type}
@@ -41,12 +54,22 @@ export default function ProductCard({ product }: Props) {
       </CardContent>
       <CardActions>
         <LoadingButton
-          loading={status.includes('pendingAddItem' + product.id)}
-          onClick={() => dispatch(addBasketItemAsync({ productId: product.id }))}
-          size="small">Add to cart
+          // We do this because now all product card components
+          // are sharing the central state status and not their individual loading state as before
+          loading={status === `pendingAddItem${product.id}`}
+          onClick={() =>
+            dispatch(addBasketItemAsync({ productId: product.id }))
+          }
+          size="small"
+        >
+          Add to Cart
         </LoadingButton>
-        <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
+        <Button component={Link} to={`/catalog/${product.id}`} size="small">
+          View
+        </Button>
       </CardActions>
     </Card>
-  )
-}
+  );
+};
+
+export default ProductCard;
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -8,19 +8,19 @@ interface Props {
     products: Product[];
 }
 
-export default function ProductList({products}: Props) {
-    const {productsLoaded} = useAppSelector(state => state.catalog);
-    return(
+export default function ProductList({ products }: Props) {
+    const { productsLoaded } = useAppSelector(state => state.catalog);
+    return (
         <Grid container spacing={4}>
-        {products.map(product =>(
-            <Grid item xs={4} key={product.id}> 
-                {!productsLoaded ? (
-                    <ProductCardSkeleton />
-                ) : (
-                    <ProductCard product={product} />
-                )}
-            </Grid>
-        ))}
-      </Grid>
+            {products.map(product => (
+                <Grid item xs={4} key={product.id}>
+                    {!productsLoaded ? (
+                        <ProductCardSkeleton />
+                    ) : (
+                        <ProductCard product={product} />
+                    )}
+                </Grid>
+            ))}
+        </Grid>
     )
 }
