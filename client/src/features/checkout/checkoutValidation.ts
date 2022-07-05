46e42169ad7e4b1d75114150b5219ea3613ae293
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-07-05 10:48:53 -0700

    Finished Video 161

--- client/src/features/checkout/checkoutValidation.ts
+++ client/src/features/checkout/checkoutValidation.ts
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
-export const validationSchema = yup.object({
+export const validationSchema = [
+    yup.object({
         fullname: yup.string().required('Full name is required'),
         address1: yup.string().required('Address line 1 is required'),
         address2: yup.string().required('Address line 2 is required'),
@@ -8,4 +9,9 @@ export const validationSchema = yup.object({
         state: yup.string().required(),
         zip: yup.string().required(),
         country: yup.string().required(),
+    }),
+    yup.object(),
+    yup.object({
+        nameOnCard: yup.string().required()
     })
+] 
