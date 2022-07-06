e48ef73a11edaa345556bfacb0565d58762de849
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-07-06 11:52:06 -0700

    Finished Video 184

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,27 +1,26 @@
-import { Container, createTheme, CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/system";
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router";
 import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
-import Header from "./header";
-import 'react-toastify/dist/ReactToastify.css'
-import ServerError from "../errors/ServerErrors";
+import 'react-toastify/dist/ReactToastify.css';
 import NotFound from "../errors/NotFound";
 import BasketPage from "../../features/basket/BasketPage";
 import LoadingComponent from "./LoadingComponent";
 import { useAppDispatch } from "../store/configureStore";
 import { fetchBasketAsync } from "../../features/basket/basketSlice";
-import Register from "../../features/account/Register";
 import Login from "../../features/account/Login";
+import Register from "../../features/account/Register";
 import { fetchCurrentUser } from "../../features/account/accountSlice";
 import PrivateRoute from "./PrivateRoute";
 import Orders from "../../features/orders/Orders";
 import CheckoutWrapper from "../../features/checkout/CheckoutWrapper";
+import ServerError from "../errors/ServerErrors";
+import Header from "./header";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -32,10 +31,10 @@ function App() {
       await dispatch(fetchCurrentUser());
       await dispatch(fetchBasketAsync());
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }, [dispatch])
- 
+
   useEffect(() => {
     initApp().then(() => setLoading(false));
   }, [initApp])
@@ -55,33 +54,34 @@ function App() {
     setDarkMode(!darkMode);
   }
 
-  if (loading) return <LoadingComponent message='Initializing app...'/>
-  
+  if (loading) return <LoadingComponent message='Initializing app...' />
 
   return (
     <ThemeProvider theme={theme}>
-      <ToastContainer position='bottom-right' hideProgressBar theme="colored"/>
-      <CssBaseline/>
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
-      <Container>
-      <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/catalog' component={Catalog}/>
-      <Route path='/catalog/:id' component={ProductDetails}/>
-      <Route path='/about' component={AboutPage}/>
-      <Route path='/contact' component={ContactPage}/>
-      <Route path='/server-error' component={ServerError}/>
-      <Route path='/basket' component={BasketPage}/>
-      <PrivateRoute path='/checkout' component={CheckoutWrapper}/>
-      <PrivateRoute path='/orders' component={Orders}/>
-      <Route path='/login' component={Login}/>
-      <Route path='/register' component={Register}/>
-      <Route component={NotFound}/>
-      </Switch>
-      </Container>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Route exact path='/' component={HomePage} />
+      <Route path={'/(.+)'} render={() => (
+        <Container  sx={{ mt: 4 }}>
+          <Switch>
+            <Route exact path='/catalog' component={Catalog} />
+            <Route path='/catalog/:id' component={ProductDetails} />
+            <Route path='/about' component={AboutPage} />
+            <Route path='/contact' component={ContactPage} />
+            <Route path='/server-error' component={ServerError} />
+            <Route path='/basket' component={BasketPage} />
+            <PrivateRoute path='/checkout' component={CheckoutWrapper} />
+            <PrivateRoute path='/orders' component={Orders} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+            <Route component={NotFound} />
+          </Switch>
+        </Container>
+      )} />
+
     </ThemeProvider>
   );
 }
 
 export default App;
- 
