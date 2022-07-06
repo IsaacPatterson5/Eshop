aa073a82e8ef03d71c02edb58439551a7b9457f1
IsaacPatterson5 <isaac.patterson@hgs.hiddengeniusproject.org>
2022-07-06 12:55:25 -0700

    Finished Video 186

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,5 +1,10 @@
+using System;
 using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace API.Middleware
 {
